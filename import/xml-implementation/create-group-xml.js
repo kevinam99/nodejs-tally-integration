6f6ea72d85c4bc258ae979b56fb7f80c2eca4d64
Kevin A Mathew <30661278+kevinam99@users.noreply.github.com>
2019-01-15 00:17:25 +0530

    Add files via upload

---
+++ import/xml-implementation/create-group-xml.js
@@ -0,0 +1,41 @@
+var request = require("request");
+
+var options = { method: 'POST',
+  url: 'http://localhost:9002',
+  headers: 
+   {
+     'cache-control': 'no-cache',
+     'Content-Type': 'text/xml' },
+  body: '<!--XML tags to createsa Group named My Debtors under Sundry Debtors-->'+
+  '<ENVELOPE>'+
+  '   <HEADER>'+
+  '       <TALLYREQUEST>Import Data</TALLYREQUEST>'+
+  '   </HEADER>'+
+  '   <BODY>'+
+  '       <IMPORTDATA>'+
+  '             <REQUESTDESC>'+
+  '                  <REPORTNAME>All Masters</REPORTNAME>'+
+  '             </REQUESTDESC>'+
+  '             <REQUESTDATA>'+
+  '                   <TALLYMESSAGE xmlns:UDF="TallyUDF">'+
+  '                       <GROUP NAME="My Debtors-Group" ACTION="Create">'+
+  '                            <NAME.LIST>'+
+  '                                 <NAME>My Debtors Name</NAME>'+
+  '                            </NAME.LIST>'+
+  '                            <PARENT>Sundry Debtors</PARENT>'+
+  '                            <ISSUBLEDGER>No</ISSUBLEDGER>'+
+  '                            <ISBILLWISEON>No</ISBILLWISEON>'+
+  '                            <ISCOSTCENTRESON>No</ISCOSTCENTRESON>'+
+  '                       </GROUP>'+
+  '                   </TALLYMESSAGE>'+
+  '             </REQUESTDATA>'+
+  '       </IMPORTDATA>'+
+  '   </BODY>'+
+  '</ENVELOPE>' 
+  };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});

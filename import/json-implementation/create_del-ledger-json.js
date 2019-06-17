a9b5a4357fe42830cdbb75b0de706f21b4e70926
Kevin A Mathew <kevinam99@gmail.com>
2019-06-17 17:52:14 +0530

    json implemented

--- import/json-implementation/create_del-ledger-json.js
+++ import/json-implementation/create_del-ledger-json.js
@@ -1,62 +1,31 @@
 var request = require("request");
+var convert = require("xml-js");
 
 function create_del_ledger(json)
 {
+    var conversion = {compact: true, ignoreComment: true, spaces: 4};
+    var xml = convert.json2xml(json, conversion);
     var options = { method: 'POST',
-  url: 'http://localhost:9002',
-  headers: 
-   { 'cache-control': 'no-cache',
-     Connection: 'keep-alive',
-     'content-length': '772',
-     'accept-encoding': 'gzip, deflate',
-     Host: 'localhost:9002',
-     
-     'Cache-Control': 'no-cache',
-     Accept: '*/*',
-     
-     'Content-Type': 'application/xml' },
-  body: '<ENVELOPE>' +
-        '    <HEADER>'+
-        '        <TALLYREQUEST>Import Data</TALLYREQUEST>'+
-        '    </HEADER>'+
-        '    <BODY>'+
-        '       <IMPORTDATA>'+
-        '           <REQUESTDESC>'+
-        '                  <REPORTNAME>All Masters</REPORTNAME>'+
-        '                  <STATICVARIABLES>'+
-        '                        <SVCURRENTCOMPANY>Test</SVCURRENTCOMPANY>'+
-        '                  </STATICVARIABLES>'+
-        '           </REQUESTDESC>'+
-        '       <REQUESTDATA>'+
-        '           <TALLYMESSAGE xmlns:UDF="TallyUDF">'+
-        '           <LEDGER NAME="Test-Ledger1" Action = "Create">'+
-        '               <STATENAME> Test state </STATENAME>'+
-        '               <PARENT>Bank Accounts</PARENT>'+
-        '               <OPENINGBALANCE> 1033 </OPENINGBALANCE>'+
-        '               <LANGUAGENAME.LIST>'+
-        '                   <NAME.LIST TYPE="String">'+
-        '                       <NAME>Test-Ledger1</NAME>'+
-        '                   </NAME.LIST>'+
-        '               </LANGUAGENAME.LIST>'+
-        '           </LEDGER>'+
-        '           </TALLYMESSAGE>'+
-        '       </REQUESTDATA> '+ 
-        '       </IMPORTDATA>'+
-        ' </BODY>'+
-        '</ENVELOPE>' };
+    url: 'http://localhost:9002',
+    headers: 
+    { 'cache-control': 'no-cache',
+      'Content-Type': 'text/xml' },
+    body: xml };
 
-request(options, function (error, response, body) {
-  if (error) throw new Error(error);
+    request(options, function (error, response, body) {
+    if (error) throw new Error(error);
 
-  console.log(body);
-});
+    console.log(body);
+    });
+
+    
 
 }
 
 
 function setProperties(action, company, ledgerName, state, openingBalance, parent)
 {
-    body: '<ENVELOPE>' +
+    var xml_body =  '<ENVELOPE>' +
         '    <HEADER>'+
         '        <TALLYREQUEST>Import Data</TALLYREQUEST>'+
         '    </HEADER>'+
@@ -85,4 +54,11 @@ function setProperties(action, company, ledgerName, state, openingBalance, paren
         '       </IMPORTDATA>'+
         ' </BODY>'+
         '</ENVELOPE>';
+    
+    
+
+    var json = convert.xml2json(xml_body, {compact: true, spaces: 4});
+    create_del_ledger(json);
 }
+
+setProperties('Delete', 'Test', 'Test-Ledger1', 'Karnataka', 1033, 'Bank Accounts');

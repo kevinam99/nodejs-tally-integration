a2d1ba33807997065b312263fff8cd0802abdb7c
Kevin A Mathew <kevinam99@gmail.com>
2019-06-18 10:54:14 +0530

    Minor changes. testing version control in VS Code

--- cvt2json.js
+++ cvt2json.js
@@ -1,21 +1,33 @@
 var convert = require("xml-js");
-var xml = '<ENVELOPE>'+
-'          <HEADER>'+
-'               <TALLYREQUEST>Export Data</TALLYREQUEST>'+
-'           </HEADER>'+
-'           <BODY>'+
-   '           <EXPORTDATA>'+
-       '           <REQUESTDESC>'+
-           '           <REPORTNAME>List of Accounts</REPORTNAME>	'+
-           '           <STATICVARIABLES>'+
-              '                 <EXPLODEFLAG>Yes</EXPLODEFLAG>'+
-               '               <SVEXPORTFORMAT>$$SysName:SDF</SVEXPORTFORMAT>'+
-               '               <ACCOUNTTYPE>All Inventory Masters</ACCOUNTTYPE> '+
-           '           </STATICVARIABLES>'+
-       '           </REQUESTDESC>'+
-   '           </EXPORTDATA>'+
-'           </BODY>'+
-'      </ENVELOPE>'
+var xml = '<ENVELOPE>' +
+        '    <HEADER>'+
+        '        <TALLYREQUEST>Import Data</TALLYREQUEST>'+
+        '    </HEADER>'+
+        '    <BODY>'+
+        '       <IMPORTDATA>'+
+        '           <REQUESTDESC>'+
+        '                  <REPORTNAME>All Masters</REPORTNAME>'+
+        '                  <STATICVARIABLES>'+
+        '                        <SVCURRENTCOMPANY>Test</SVCURRENTCOMPANY>'+
+        '                  </STATICVARIABLES>'+
+        '           </REQUESTDESC>'+
+        '       <REQUESTDATA>'+
+        '           <TALLYMESSAGE xmlns:UDF="TallyUDF">'+
+        '           <LEDGER NAME="Test-Ledger1" Action = "Create">'+
+        '               <STATENAME> Test state </STATENAME>'+
+        '               <PARENT>Bank Accounts</PARENT>'+
+        '               <OPENINGBALANCE> 1033 </OPENINGBALANCE>'+
+        '               <LANGUAGENAME.LIST>'+
+        '                   <NAME.LIST TYPE="String">'+
+        '                       <NAME>Test-Ledger1</NAME>'+
+        '                   </NAME.LIST>'+
+        '               </LANGUAGENAME.LIST>'+
+        '           </LEDGER>'+
+        '           </TALLYMESSAGE>'+
+        '       </REQUESTDATA> '+ 
+        '       </IMPORTDATA>'+
+        ' </BODY>'+
+        '</ENVELOPE>'
 
 var json = convert.xml2json(xml, {compact: true, spaces: 4});
 console.log(json);
